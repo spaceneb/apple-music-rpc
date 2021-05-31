44129b75cfe86b1d62112898be172a3b41b738b4
spaceneb <spaceneb@tutanota.com>
2021-05-30 21:22:41 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+const exec = require("child_process").exec;
+const RPC = require('discord-rpc');
+const clientId = '848701710553710604';
+const client = new RPC.Client({ transport: 'ipc' });
+const cron = require('node-cron');
+
+function setTime(sec) {
+    var t = new Date();
+    t.setSeconds(t.getSeconds() - sec);
+    return t.getTime()
+}
+
+function refreshRPC() {
+    var activity = {};
+    exec('./data.sh', (err, stdout, stderr) => {
+        var data = JSON.parse(stdout)
+        activity.details = data.name;
+        activity.state = `${data.artist} - ${data.album}`;
+        activity.largeImageKey = 'icon';
+        activity.largeImageText = 'Apple Music';
+        activity.startTimestamp = setTime(data.position)
+        activity.buttons = [{label:"Search on Apple Music",url:`https://music.apple.com/us/search?term=${encodeURIComponent(data.name)}`},{label:"Search on Spotify",url:`https://open.spotify.com/search/${encodeURIComponent(data.name)}`}]
+        client.setActivity(activity);
+    });
+}
+client.on('ready', () => {
+    console.log('Authorized for', client.user.username);
+    refreshRPC();
+});
+cron.schedule('*/2 * * * * * *', () => {
+    refreshRPC();
+});
+
+client.login({ clientId });
